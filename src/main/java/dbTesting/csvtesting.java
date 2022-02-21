704c110e6498d6348d63b73364c43e3e97fb9317
tctfox <tctfoxi@gmail.com>
2022-02-21 18:57:07 +0100

    db6

---
+++ src/main/java/dbTesting/csvtesting.java
@@ -0,0 +1,7 @@
+package dbTesting;
+
+public class csvtesting {
+
+    //create table test12(Match_id,Game_start_time_utc,Game_version,Player,Deck_code,Game_outcome,Order_of_play,Total_turn_count,Faction_1,Faction_2,Archetype,Is_master,Region)
+
+}
